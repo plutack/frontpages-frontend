ab78e0dd56038fb4b6d6d84931b8a150abecf013
Talut Salako <salakotalut@gmail.com>
2024-09-22 04:47:19 +0100

    refactor: remove unused functions

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -20,15 +20,7 @@ const fetchPapers = async (): Promise<PaperDetailsType[]> => {
   return data.result;
 };
 
-const getYesterdayDate = () => {
-  const today = new Date();
-  today.setDate(today.getDate() - 1);
-  const year = today.getFullYear();
-  const month = String(today.getMonth() + 1).padStart(2, '0'); 
-  const day = String(today.getDate()).padStart(2, '0');
 
-  return `${year}-${month}-${day}`;
-};
 
 
 
