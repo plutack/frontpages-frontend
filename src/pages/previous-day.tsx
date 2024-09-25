a00aaa3ffe0ba225444ff09a125db0c4f7550e15
Talut Salako <salakotalut@gmail.com>
2024-09-25 22:23:41 +0100

    fix: change header

--- src/pages/previous-day.tsx
+++ src/pages/previous-day.tsx
@@ -67,7 +67,7 @@ function Yesterday() {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-6 text-center">Today's Papers</h1>
+      <h1 className="text-2xl font-bold mb-6 text-center">Yesterday's Papers</h1>
       <div className="grid gap-4 items-center justify-center md:grid-cols-2 lg:grid-cols-3">
         {data.map((paper) => (
           <Link key={paper.id} to={`/newspapers/${paper.id}`} className="no-underline">
